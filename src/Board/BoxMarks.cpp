5fc06dfb2df44c97603d77eb71414349f9a38e28
digimokan <digimokan@users.noreply.github.com>
2019-02-06 11:51:27 -0600

    Use emplace, not insert, in BoxMarks unord_map

--- src/Board/BoxMarks.cpp
+++ src/Board/BoxMarks.cpp
@@ -28,7 +28,7 @@ purpose:  map of box_num -> marking_player
 
 BoxMarks::BoxMarks (std::size_t board_dimensions) {
   for (std::size_t i = 0; i < (board_dimensions * board_dimensions); ++i)
-    this->unmarked_boxnums.insert(this->unmarked_boxnums.end(), i);
+    this->unmarked_boxnums.emplace(i);
 }
 
 /*******************************************************************************
@@ -56,9 +56,9 @@ void BoxMarks::mark (std::size_t box_num, Player player) {
   assert(this->marks.count(box_num) == 0);
   assert(this->unmarked_boxnums.count(box_num) == 1);
   assert(this->marked_boxnums.count(box_num) == 0);
-  this->marks.insert(std::make_pair(box_num, player));
+  this->marks.emplace(std::make_pair(box_num, player));
   this->unmarked_boxnums.erase(box_num);
-  this->marked_boxnums.insert(box_num);
+  this->marked_boxnums.emplace(box_num);
 }
 
 std::size_t BoxMarks::get_num_unmarked () const {
