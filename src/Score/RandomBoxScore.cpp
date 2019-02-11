852504d4f2163a5bc7b692b10bf1747429e789c4
digimokan <digimokan@users.noreply.github.com>
2019-02-11 00:05:14 -0600

    Add RandomBoxScore class: each box worth 1-5 pts

---
+++ src/Score/RandomBoxScore.cpp
@@ -0,0 +1,70 @@
+/*******************************************************************************
+module:   RandomBoxScore
+author:   digimokan
+date:     10 FEB 2019
+purpose:  given board, return [ Human - Computer ] score
+          --> each completed box is worth a random point (from 1-5)
+*******************************************************************************/
+
+/*******************************************************************************
+* SYSTEM INCLUDES
+*******************************************************************************/
+
+#include <cstddef>
+#include <cstdint>
+#include <random>
+#include <vector>
+
+/*******************************************************************************
+* USER INCLUDES
+*******************************************************************************/
+
+#include "Board.hpp"
+#include "Player.hpp"
+#include "RandomBoxScore.hpp"
+
+/*******************************************************************************
+* CONSTRUCTORS
+*******************************************************************************/
+
+RandomBoxScore::RandomBoxScore (std::size_t dimensions) {
+  this->init_box_scores(dimensions);
+}
+
+/*******************************************************************************
+* BASE / DERIVED METHODS
+*******************************************************************************/
+
+int64_t RandomBoxScore::calc_score (const Board& board) {
+  int64_t oscore{ this->calc_player_score(Player::ONE, board) };
+  int64_t cscore{ this->calc_player_score(Player::COMPUTER, board) };
+  return (oscore - cscore);
+}
+
+/*******************************************************************************
+* SPECIALIZED METHODS
+*******************************************************************************/
+
+int64_t RandomBoxScore::get_random_box_score (std::size_t box_num) const {
+  return this->box_scores.at(box_num);
+}
+
+/*******************************************************************************
+* HELPER METHODS
+*******************************************************************************/
+
+void RandomBoxScore::init_box_scores (std::size_t dimensions) {
+  const std::size_t num_boxes{ dimensions * dimensions };
+  std::default_random_engine generator{ num_boxes };
+  std::uniform_int_distribution<int64_t> distribution{1, 5};
+  for (std::size_t box_num{0}; box_num < num_boxes; ++box_num)
+    this->box_scores.emplace_back(distribution(generator));
+}
+
+int64_t RandomBoxScore::calc_player_score (Player player, const Board& board) const {
+  int64_t score{ 0 };
+  auto add_to_sum{ [this, &score] (auto box_num) { score += this->box_scores.at(box_num); } };
+  board.for_each_player_boxmark(player, add_to_sum);
+  return score;
+}
+
---
+++ src/Score/RandomBoxScore.hpp
@@ -0,0 +1,76 @@
+/*******************************************************************************
+module:   RandomBoxScore
+author:   digimokan
+date:     10 FEB 2019
+purpose:  given board, return [ Human - Computer ] score
+--> each completed box is worth a random point (from 1-5)
+*******************************************************************************/
+
+#ifndef RANDOM_BOX_SCORE_HPP
+#define RANDOM_BOX_SCORE_HPP 1
+
+/*******************************************************************************
+* SYSTEM INCLUDES
+*******************************************************************************/
+
+#include <cstddef>
+#include <cstdint>
+#include <vector>
+
+/*******************************************************************************
+* USER INCLUDES
+*******************************************************************************/
+
+#include "ScoreIface.hpp"
+
+/*******************************************************************************
+* FORWARD DECLARES
+*******************************************************************************/
+
+class Board;
+enum class Player;
+
+/*******************************************************************************
+* INTERFACE
+*******************************************************************************/
+
+class RandomBoxScore : public ScoreIface {
+
+public:
+
+  // constructors
+  explicit RandomBoxScore (std::size_t dimensions);
+
+  // destructor
+  ~RandomBoxScore () override = default;
+
+  // operators
+  RandomBoxScore (const RandomBoxScore& in) = default;
+  RandomBoxScore& operator= (const RandomBoxScore& rh) = default;
+  RandomBoxScore (RandomBoxScore&& in) = default;
+  RandomBoxScore& operator= (RandomBoxScore&& rh) = default;
+
+  // base / derived methods
+  int64_t calc_score (const Board& board) final;
+
+  // specialized methods
+  int64_t get_random_box_score (std::size_t box_num) const;
+
+private:
+
+  // helper methods
+  void init_box_scores (std::size_t dimensions);
+  int64_t calc_player_score (Player player, const Board& board) const;
+
+
+  // fields
+  std::vector<int64_t> box_scores;
+
+};
+
+/*******************************************************************************
+* END
+*******************************************************************************/
+
+#endif // RANDOM_BOX_SCORE_HPP
+
